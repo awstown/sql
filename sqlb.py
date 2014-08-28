f5544e7081ee5061ec9b4b3bdc17349776aa2a50
Dwight Townsend <dwtownse@calpoly.edu>
2014-08-28 08:38:44 -0700

    Inserting Data - 2

--- sqlb.py
+++ sqlb.py
@@ -3,19 +3,7 @@
 
 # import the SQLite3 library
 import sqlite3
-
-# create a new database if the database doesn't already exist
-conn = sqlite3.connect("new.db")
-
-# get a cursor object used to execute SQL commands
-cursor = conn.cursor()
-
-# insert data
-cursor.execute("INSERT INTO population VALUES('New York City', 'NY', 820000)")
-cursor.execute("INSERT INTO population VALUES('San Francisco City', 'NY', 800000)")
-
-# commit the changes
-conn.commit()
-
-# close the database connection
-conn.close()
+with sqlite3.connect("new.db") as connection:
+    c = connection.cursor()
+    c.execute("INSERT INTO population VALUES('New York City', 'NY', 820000)")
+    c.execute("INSERT INTO population VALUES('San Francisco City', 'NY', 800000)")
