6f563a5649ce0a820328026ebaf5888f93c94179
Dwight Townsend <dwtownse@calpoly.edu>
2014-08-28 10:29:14 -0700

    assignment 3b

--- assignment3b.py
+++ assignment3b.py
@@ -1,3 +1,26 @@
 # Assignment 3b - prompt the user
 
-# import the 
+# import the sqlite3 lib
+import sqlite3
+
+# create the connection object
+conn = sqlite3.connect("newnum.db")
+
+# create cursor object used to execute SQL commands
+cursor = conn.cursor()
+
+prompt = """
+Select the operation that you want to perform [1-5]:
+1. Average
+2. Max
+3. Min
+4. Sum
+5. Exit
+"""
+
+# loop until user enters a valid operation number [1-5]
+while True
+    # get user input
+    x = raw_input(prompt)
+
+    ........
